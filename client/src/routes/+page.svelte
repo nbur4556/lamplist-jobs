d6ecbc3e9633b74917c6a424ab37e3583825c533
nbur4556 <nbur4556@gmail.com>
2023-01-09 20:26:19 -0600

    feat: inital form for entering employers into job list

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+  let jobList: Array<string> = [];
+  let entry: string = '';
+
+  const addEntry = () => {
+    //TODO: Keep selection on entry input after submit for rapid data entry
+    jobList = [...jobList, entry];
+    entry = '';
+  }
+</script>
+
+<h1>Welcome to Lamp List Jobs!</h1>
+
+<form on:submit={addEntry}>
+  <label>
+    List 40+ employers that you would like to work for.
+    <input type="text" bind:value={entry} />
+  </label>
+  
+  <button type="submit">Submit</button>
+</form>
+
+<ul>
+  {#each jobList as job}
+    <li>{job}</li>
+  {/each}
+</ul>
