b54c8e584f57f48c7419c80287d6dac736cff407
nbur4556 <nbur4556@gmail.com>
2023-01-09 21:46:30 -0600

    ref: remove console log

--- client/src/store.ts
+++ client/src/store.ts
@@ -3,8 +3,6 @@ import { writable } from 'svelte/store';
 const createJobListStore = () => {
   const { subscribe, update } = writable<Array<string>>([]);
 
-  console.log('custom store');
-
   return {
     subscribe,
     addEntry: (entry: string) => update(state => [...state, entry]),
