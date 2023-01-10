721fa68658b070f70b75165eb444536b23976ecd
nbur4556 <nbur4556@gmail.com>
2023-01-09 20:32:45 -0600

    feat: keep track of current and remaining job list count

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,24 +2,30 @@
   let jobList: Array<string> = [];
   let entry: string = '';
 
+  const targetListSize = 10;
+
   const addEntry = () => {
     //TODO: Keep selection on entry input after submit for rapid data entry
     jobList = [...jobList, entry];
     entry = '';
   }
+
+  $: jobCount = jobList.length;
+  $: remainingCount = (jobCount < targetListSize) ? -(jobCount - targetListSize) : 0;
 </script>
 
 <h1>Welcome to Lamp List Jobs!</h1>
 
 <form on:submit={addEntry}>
   <label>
-    List 40+ employers that you would like to work for.
+    List {remainingCount}+ employers that you would like to work for
     <input type="text" bind:value={entry} />
   </label>
   
   <button type="submit">Submit</button>
 </form>
 
+<h2>Job List ({jobCount})</h2>
 <ul>
   {#each jobList as job}
     <li>{job}</li>
