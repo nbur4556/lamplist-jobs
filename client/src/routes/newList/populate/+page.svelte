26a3b754735561ee755daae96c1fcd2bf59d38f1
nbur4556 <nbur4556@gmail.com>
2023-01-10 19:05:32 -0600

    feat: create and cycle through job list population steps

--- client/src/routes/newList/populate/+page.svelte
+++ client/src/routes/newList/populate/+page.svelte
@@ -3,10 +3,44 @@
   import {JobListStore} from '../../../store';
 
   let entryId = 0;
+  let step: 0 | 1 | 2 = 0; 
 
-  const nextEntry = () => entryId += 1;
-  const preEntry = () => entryId += -1;
+  const nextEntry = () => {
+    const atMaxEntry = (entryId >= $JobListStore.length - 1);
+
+    if(atMaxEntry && step >= 2) {
+      return;
+    }
+    else if(atMaxEntry) {
+      entryId = 0;
+      step += 1;
+      return;
+    }
+
+    entryId += 1;
+  }
+
+  const preEntry = () => {
+    if(entryId <= 0 && step <= 0) {
+      return;
+    }
+    else if (entryId <= 0) {
+      entryId = $JobListStore.length - 1;
+      step += -1;
+      return;
+    }
+
+    entryId += -1;
+  }
 </script>
+
+{#if step === 0}
+  <p>Enter Contacts</p>
+{:else if step === 1}
+  <p>Enter Interest</p>
+{:else if step === 2}
+  <p>Enter Posting</p>
+{/if}
 
 <Entry job={$JobListStore[entryId]} index={entryId} />
 
