6b0928c1d462d726d8fc287e3aad2ba15a1fa1f1
nbur4556 <nbur4556@gmail.com>
2023-01-10 18:34:01 -0600

    feat: create job lists from new list route

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,29 +1,3 @@
-<script lang='ts'>
-  import JobList from '../lib/JobList/JobList.svelte';
-  import {JobListStore} from '../store';
-
-  let entry: string = '';
-  const targetListSize = 40;
-
-  const addEntry = () => {
-    //TODO: Keep selection on entry input after submit for rapid data entry
-    JobListStore.addEntry({company: entry});
-    entry = '';
-  }
-
-  $: jobLength = $JobListStore.length
-  $: remainingCount = (jobLength < targetListSize) ? -(jobLength - targetListSize) : 0;
-</script>
-
 <h1>Welcome to Lamp List Jobs!</h1>
 
-<form on:submit={addEntry}>
-  <label>
-    List {remainingCount}+ employers that you would like to work for
-    <input type="text" bind:value={entry} />
-  </label>
-  
-  <button type="submit">Submit</button>
-</form>
-
-<JobList />
+<a href="/newList">New Lamp List</a>
---
+++ client/src/routes/newList/+page.svelte
@@ -0,0 +1,30 @@
+<script lang='ts'>
+  import JobList from '../../lib/JobList/JobList.svelte';
+  import {JobListStore} from '../../store';
+
+  let entry: string = '';
+  const targetListSize = 40;
+
+  const addEntry = () => {
+    //TODO: Keep selection on entry input after submit for rapid data entry
+    JobListStore.addEntry({company: entry});
+    entry = '';
+  }
+
+  $: jobLength = $JobListStore.length
+  $: remainingCount = (jobLength < targetListSize) ? -(jobLength - targetListSize) : 0;
+</script>
+
+<h1>Welcome to Lamp List Jobs!</h1>
+<a href="/">back</a>
+
+<form on:submit={addEntry}>
+  <label>
+    List {remainingCount}+ employers that you would like to work for
+    <input type="text" bind:value={entry} />
+  </label>
+  
+  <button type="submit">Submit</button>
+</form>
+
+<JobList />
