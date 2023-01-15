e767815b12be6167627eb73a3dd3709d82dc3f14
nbur4556 <nbur4556@gmail.com>
2023-01-15 12:17:07 -0600

    feat: create api route for adding job entries to the server

--- server/Controllers/JobListController.cs
+++ server/Controllers/JobListController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 using server.Models;
 
@@ -8,16 +10,31 @@ namespace server.Controllers;
 [Route("api/[controller]")]
 public class JobListController : ControllerBase
 {
-  JobEntry[] JobEntryList = {
-    new JobEntry("Test Company 1"),
-    new JobEntry("Test Company 2", contact: "Test Contact"),
-    new JobEntry("Test Company 3", interest: 3),
-    new JobEntry("Test Company 4", posting: "Test Posting")
-  };
+  List<JobEntry> JobEntryData = new List<JobEntry>();
+
+  public JobListController()
+  {
+    JobEntryData.Add(new JobEntry("Test Company 1"));
+    JobEntryData.Add(new JobEntry("Test Company 2", contact: "Test Contact"));
+    JobEntryData.Add(new JobEntry("Test Company 3", interest: 3));
+    JobEntryData.Add(new JobEntry("Test Company 4", posting: "Test Posting"));
+  }
 
   [HttpGet]
   public ActionResult<JobEntry[]> GetJobList()
   {
-    return JobEntryList;
+    return JobEntryData.ToArray<JobEntry>();
   }
+
+  [HttpPost]
+  public ActionResult<JobEntry> PostJobEntryToList()
+  {
+    JobEntry jobEntry = new JobEntry("Test Company 5");
+    JobEntryData.Add(jobEntry);
+    return jobEntry;
+  }
+
+  //TODO: Update job entry route/
+
+  //TODO: Delete job entry route
 }
