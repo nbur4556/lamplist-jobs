a102a77b1ac42c273a839c55b3c1f6426ab614cc
nbur4556 <nbur4556@gmail.com>
2023-01-16 08:47:14 -0600

    ref: format client

--- client/src/store/JobListStore.ts
+++ client/src/store/JobListStore.ts
@@ -1,6 +1,11 @@
 import { writable } from 'svelte/store';
 
-import { addJobEntries, deleteJobEntries, fetchJobEntries, updateJobEntries } from '@src/features/jobList';
+import {
+	addJobEntries,
+	deleteJobEntries,
+	fetchJobEntries,
+	updateJobEntries
+} from '@src/features/jobList';
 
 export interface JobEntry {
 	company: string;
@@ -19,7 +24,7 @@ const createJobListStore = () => {
 		fetchEntries: () => fetchJobEntries(update),
 		updateEntry: (entry: Partial<JobEntry>, id: number) => updateJobEntries(id, entry, update),
 		addEntry: (entry: JobEntry) => addJobEntries(entry, update),
-		removeEntry: (id: number) => deleteJobEntries(id, update),
+		removeEntry: (id: number) => deleteJobEntries(id, update)
 	};
 };
 
