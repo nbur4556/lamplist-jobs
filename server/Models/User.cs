26267d1969365f80dfded564a38b7bfd7655a33e
nbur4556 <nbur4556@gmail.com>
2023-01-31 18:33:04 -0600

    init: initial migration for identity tables in database

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -6,12 +6,7 @@ namespace server.Models;
 [PrimaryKey(nameof(Id))]
 public class User : IdentityUser<Guid>
 {
-  public string Username { get; set; }
 
-  public User(string username)
-  {
-    Username = username;
-  }
 }
 
 public class UserRole : IdentityRole<Guid>
