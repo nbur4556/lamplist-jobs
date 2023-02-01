3e7246354855988e83c6f2c75c8a5d1162bdf57c
nbur4556 <nbur4556@gmail.com>
2023-02-01 13:32:27 -0600

    feat: use password inputs on register page

---
+++ client/src/lib/Form/PasswordInput.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let value: string | undefined;
+	export let hidden = false;
+</script>
+
+<label {hidden}>
+	<slot />
+	<input type="password" bind:value {hidden} />
+</label>
--- client/src/routes/auth/register/+page.svelte
+++ client/src/routes/auth/register/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Input from '@src/lib/Form/Input.svelte';
+	import PasswordInput from '@src/lib/Form/PasswordInput.svelte';
 
 	interface FormValues {
 		userName?: string;
@@ -12,13 +13,23 @@
 		password: undefined,
 		confirmPassword: undefined
 	};
+
+	const onSubmit = () => {
+		if (formValues.password !== formValues.confirmPassword) {
+			//TODO: Display Error Message
+			console.error('Password and confirm password do not match');
+			return;
+		}
+
+		console.log(formValues);
+	};
 </script>
 
 <main>
-	<form>
+	<form on:submit={onSubmit}>
 		<Input bind:value={formValues.userName}>UserName</Input>
-		<Input bind:value={formValues.userName}>Password</Input>
-		<Input bind:value={formValues.userName}>Confirm Password</Input>
+		<PasswordInput bind:value={formValues.password}>Password</PasswordInput>
+		<PasswordInput bind:value={formValues.confirmPassword}>Confirm Password</PasswordInput>
 		<button type="submit">Submit</button>
 	</form>
 </main>
