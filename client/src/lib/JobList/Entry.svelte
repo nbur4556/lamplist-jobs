478a099d3203c0a59ec3570ca71b0ae26211da7b
nbur4556 <nbur4556@gmail.com>
2023-05-22 18:59:17 -0500

    initial style for job list entry

--- client/src/lib/JobList/Entry.svelte
+++ client/src/lib/JobList/Entry.svelte
@@ -12,3 +12,22 @@
 	<EntryCard {job} />
 	<button on:click={() => JobListStore.removeEntry(id)}>X</button>
 </li>
+
+<style lang="scss">
+	@use '../../theme/colors';
+	@use '../../theme/sizes';
+
+	li {
+		display: flex;
+		flex-direction: row;
+	}
+
+	button {
+		margin: sizes.$spacing-sm 0;
+
+		color: colors.$txt-main;
+		background-color: colors.$bg-mid;
+		border: none;
+		border-radius: 10px;
+	}
+</style>
