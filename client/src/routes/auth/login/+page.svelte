bfdf5f3a348fff5d870bb84b433fe0c73c3e698b
nbur4556 <nbur4556@gmail.com>
2023-06-07 19:20:31 -0500

    initial styling for login page

--- client/src/routes/auth/login/+page.svelte
+++ client/src/routes/auth/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Input from '@src/lib/Form/Input.svelte';
+	import PageContent from '@src/lib/UI/PageContent.svelte';
 	import PasswordInput from '@src/lib/Form/PasswordInput.svelte';
 	import { AuthStore } from '@src/store/AuthStore';
 
@@ -23,11 +24,28 @@
 	};
 </script>
 
-<main>
+<PageContent>
+	<h1>Log In</h1>
 	<a href="/">Home</a>
 	<form on:submit={onSubmit}>
 		<Input bind:value={formValues.userName}>UserName</Input>
 		<PasswordInput bind:value={formValues.password}>Password</PasswordInput>
 		<button type="submit">Submit</button>
 	</form>
-</main>
+</PageContent>
+
+<style lang="scss">
+	@use '../../../theme/sizes';
+
+	h1 {
+		font-size: sizes.$font-lg;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: sizes.$spacing-md;
+
+		width: 100%;
+	}
+</style>
