3ca80801ba95b8061e64a3d1ab1b12f0588919d7
nbur4556 <nbur4556@gmail.com>
2023-07-18 00:18:47 -0500

    create first test for token service

--- server/Tests/Services/ITokenServiceTests.cs
+++ server/Tests/Services/ITokenServiceTests.cs
@@ -1,15 +1,32 @@
+using Moq;
+using System.Security.Claims;
+using Xunit;
+
 using server.Services;
 
-namespace server.Tests;
-
-public class ITokenServiceTests 
+//! No test is available
+//TODO: Make sure that test discoverer & executors are registered and platform & framework version settings are appropriate
+namespace server.Tests
 {
-  public void CreateToken_ShouldReturnNewSecurityTokenAsString() 
+  public class ITokenServiceTests
   {
-    // Arrange
+    [Fact]
+    public void CreateToken_ShouldReturnNewSecurityTokenAsString()
+    {
+      // Arrange
+      Mock<IConfiguration> configurationMock = new Mock<IConfiguration>();
+      TokenService _tokenService = new TokenService(configurationMock.Object);
+      Claim[] claims = new[]
+      {
+        new Claim(ClaimTypes.NameIdentifier, "testId"),
+        new Claim("AccountIdentifier", "accountId"),
+      };
 
-    // Act
+      // Act
+      string tokenResult = _tokenService.CreateToken(claims);
 
-    // Assert
+      // Assert
+      Assert.Equal(tokenResult, tokenResult);
+    }
   }
 }
