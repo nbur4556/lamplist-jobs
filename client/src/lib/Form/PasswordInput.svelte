eaf8f75c3b6565390a7cccfc82b7a493f5107971
Nick Burt <nbur4556@gmail.com>
2023-08-24 20:30:55 -0500

    use component library for login page

--- client/src/lib/Form/PasswordInput.svelte
+++ client/src/lib/Form/PasswordInput.svelte
@@ -3,16 +3,7 @@
 	export let hidden = false;
 </script>
 
-<div>
-	<label {hidden}><slot /></label>
-	<!-- Placed in another div to remove 3rd party plugin icons as flex children -->
-	<div><input type="password" bind:value {hidden} /></div>
+<div class="form-control">
+	<label class="label label-text" {hidden}><slot /></label>
+	<input type="password" class="input input-bordered" bind:value {hidden} />
 </div>
-
-<style>
-	div {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-	}
-</style>
