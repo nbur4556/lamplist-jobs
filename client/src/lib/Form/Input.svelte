5c759e89318658b4e8ce0d6b016de84c48281a2e
Nick Burt <nbur4556@gmail.com>
2023-08-26 00:09:27 -0500

    use component library for new list page

--- client/src/lib/Form/Input.svelte
+++ client/src/lib/Form/Input.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let value: string | undefined;
+	export let name = 'name';
 	export let hidden = false;
 </script>
 
 <div class="form-control">
-	<label class="label label-text" {hidden}><slot /></label>
-	<input type="text" class="input input-bordered" bind:value {hidden} />
+	<label class="label label-text" for={name} {hidden}><slot /></label>
+	<input class="input input-bordered" {name} type="text" bind:value {hidden} />
 </div>
