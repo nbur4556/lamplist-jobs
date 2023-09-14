82ce9d86910f3b5b0deb0addd26db6fdf2ee9085
Nick Burt <nbur4556@gmail.com>
2023-09-14 13:04:15 -0500

    create action for collapsing job entry cards

--- client/src/lib/JobList/EntryCard.svelte
+++ client/src/lib/JobList/EntryCard.svelte
@@ -2,6 +2,8 @@
 	import type { JobEntry } from '@src/store/JobListStore';
 	export let job: JobEntry;
 	export let collapsed = false;
+
+	const toggleCollapsed = () => (collapsed = !collapsed);
 </script>
 
 <section class="card w-full bg-primary">
@@ -10,10 +12,13 @@
 			<h3>{job.company}</h3>
 			<div class="card-actions">
 				<slot name="actions" />
+				<button class="btn btn-outline btn-xs" on:click={toggleCollapsed}>
+					{#if !collapsed} - {:else} + {/if}
+				</button>
 			</div>
 		</div>
 
-		{#if collapsed === false}
+		{#if !collapsed}
 			<p>
 				Contact: {#if job.contact}{job.contact}{/if}
 			</p>
