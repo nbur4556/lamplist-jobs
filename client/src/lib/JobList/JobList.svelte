267ba68f560e1e383e04eecc88cc2005fbbaab72
Nick Burt <nbur4556@gmail.com>
2023-09-14 13:10:06 -0500

    create job list action for collapsing and expanding all cards at once

--- client/src/lib/JobList/JobList.svelte
+++ client/src/lib/JobList/JobList.svelte
@@ -2,15 +2,22 @@
 	import EntryCard from './EntryCard.svelte';
 	import { JobListStore } from '@src/store/JobListStore';
 
+	let collapsedEntries = false;
+
+	const toggleCollapsedEntries = () => (collapsedEntries = !collapsedEntries);
+
 	$: jobCount = $JobListStore.length;
 </script>
 
 <section class="flex flex-col self-stretch">
 	<p class="self-end">Jobs ({jobCount})</p>
+	<button class="btn btn-outline btn-sm self-end" on:click={toggleCollapsedEntries}>
+		{#if !collapsedEntries}Collapse{:else}Expand{/if}
+	</button>
 	<ul>
 		{#each $JobListStore as job}
 			<li class="py-2">
-				<EntryCard {job} collapsed={false}>
+				<EntryCard {job} collapsed={collapsedEntries}>
 					<button
 						class="btn btn-outline btn-xs"
 						slot="actions"
