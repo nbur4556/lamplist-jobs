8580f6bd1d1ed0e4aabc52fc3d841367747e5892
Nick Burt <nbur4556@gmail.com>
2023-10-04 22:06:40 -0500

    create a json object for categories content

--- client/src/routes/newList/+page.svelte
+++ client/src/routes/newList/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
+	import categories from '@src/content/listCategories.json';
 	import Input from '@src/lib/Form/Input.svelte';
 	import PageContent from '@src/lib/UI/PageContent.svelte';
 	import { JobListStore } from '@src/store/JobListStore';
 
-	//TODO: Should use category text from lamplist method
-	const categories = ["dream employers", "companies with a connection", "companies actively hiring", "trending employers"];
 	let categoryIndex = 0;
 	let jobEntries = new Array<string>(10);
 
 	const addEntries = () => {
-		jobEntries.forEach(company => {
+		jobEntries.forEach((company) => {
 			JobListStore.addEntry({ company });
 		});
 		initializeCategory();
@@ -21,18 +20,18 @@
 
 	const removeFromForm = () => {
 		jobEntries = jobEntries.slice(0, jobEntries.length - 1);
-	}
+	};
 
 	const initializeCategory = () => {
 		jobEntries = new Array(10);
 
-		if (categoryIndex >= categories.length - 1){
+		if (categoryIndex >= categories.length - 1) {
 			//TODO: Should route to the populating step
 			return;
 		}
 
 		categoryIndex++;
-	}
+	};
 </script>
 
 <PageContent>
@@ -43,13 +42,14 @@
 		</ul>
 	</nav>
 
-	<p>Add entries for 10 {categories[categoryIndex]}</p>
+	<p>Add 10 entries for {categories[categoryIndex].title}</p>
+	<p>{categories[categoryIndex].description}</p>
 
 	<div class="flex self-end gap-3">
 		<button class="btn btn-outline btn-xs" on:click={removeFromForm}>-</button>
 		<button class="btn btn-outline btn-xs" on:click={addToForm}>+</button>
 	</div>
-	
+
 	<form class="flex flex-col justify-between w-full" on:submit={addEntries}>
 		{#each jobEntries as _, i}
 			<Input name={`job-${i}`} bind:value={jobEntries[i]} />
