842a43211b82244e2c9411c79960529ac2797a33
Nick Burt <nbur4556@gmail.com>
2023-10-20 21:54:21 -0500

    use close icon in close window button

---
+++ client/src/lib/Icons/CloseIcon.svelte
@@ -0,0 +1,17 @@
+<!-- Source: https://heroicons.com/ -->
+<script lang="ts">
+    export let strokeWidth = "1.5"
+    let className = "w-6 h-6";
+    export {className as class};
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width={strokeWidth}
+	stroke="currentColor"
+	class={className}
+>
+	<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg>
