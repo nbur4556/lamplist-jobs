0aa1ccd83c091df4c291f8475658512d57360aca
Nick Burt <nbur4556@gmail.com>
2023-11-22 19:47:16 -0600

    sort job list by sort term

--- client/src/lib/JobList/JobList.svelte
+++ client/src/lib/JobList/JobList.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
 	import TrashIcon from '@src/lib/Icons/TrashIcon.svelte';
-	import { JobListStore } from '@src/store/JobListStore';
+	import { JobListStore } from '@src/store/JobListStore'; 
+	import type { JobEntry } from '@src/store/JobListStore';
 	import EntryCard from './EntryCard.svelte';
 
+	//TODO: Sort direction
 	//? What is the best way to do this type? Is it defined? KeyOf(job)
 	export let sortBy: "company" | "contact" | "interest" | "posting";
 	
 	let collapsedEntries = false;
 
 	const toggleCollapsedEntries = () => (collapsedEntries = !collapsedEntries);
+	
+	//TODO: refactor into generic typed sortObjectList
+	//TODO: refactor as a utility function
+	//TODO: thoroughly test this function
+	const sortJobList = (list: Array<JobEntry>, term: "company" | "contact" | "interest" | "posting"): Array<JobEntry> => {
+		let sortedList: Array<JobEntry> = [];
+		list.forEach(job => {
+			for(let i = 0; i < sortedList.length; i++){
+				//TODO: fix "may be undefined" type error
+				if (job[term] < sortedList[i][term]) {
+					sortedList = [...sortedList.slice(0, i), job, ...sortedList.slice(i, sortedList.length)];
+					return;
+				}
+			}
+			sortedList = [...sortedList, job];
+		});
+		return sortedList;
+	}
 
 	$: jobCount = $JobListStore.length;
+	$: sortedList = sortJobList($JobListStore, sortBy);
 </script>
 
 <section class="flex flex-col self-stretch">
-	{console.log(sortBy)}
 	<div class="flex justify-end items-center gap-3">
 		<p>Jobs ({jobCount})</p>
 		<button class="btn btn-outline btn-sm" on:click={toggleCollapsedEntries}>
@@ -22,8 +42,7 @@
 		</button>
 	</div>
 	<ul>
-		{#each $JobListStore as job}
-		{console.log(job)}
+		{#each sortedList as job}
 			<li class="py-2">
 				<EntryCard {job} collapsed={collapsedEntries}>
 					<button
