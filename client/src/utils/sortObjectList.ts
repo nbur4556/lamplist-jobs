222f550b44b5c34f20d48ee6389a33fd672fb451
Nick Burt <nbur4556@gmail.com>
2023-11-25 14:13:06 -0600

    unnest compare value function

--- client/src/utils/sortObjectList.ts
+++ client/src/utils/sortObjectList.ts
@@ -1,15 +1,15 @@
+const compareVal = <T>(a: T, b: T, reversed: boolean) => {
+	return reversed ? a > b : a < b;
+};
+
 //! Undefined values do not get sorted, should be at the end
 //TODO: thoroughly test this function
 const sortObjectList = <T>(list: Array<T>, term: keyof T, reversed: boolean): Array<T> => {
-	type Val = T[typeof term];
-	const checkComparison = (a: Val, b: Val, reversed: boolean) => {
-		return reversed ? a > b : a < b;
-	};
-
+	type V = T[typeof term];
 	let sortedList: Array<T> = [];
 	list.forEach((entry) => {
 		for (let i = 0; i < sortedList.length; i++) {
-			if (checkComparison(entry[term], sortedList[i][term], reversed)) {
+			if (compareVal<V>(entry[term], sortedList[i][term], reversed)) {
 				sortedList = [...sortedList.slice(0, i), entry, ...sortedList.slice(i, sortedList.length)];
 				return;
 			}
